438aeaf74ef95bfc06e0a4bf7e440542daa504c2
lifengqi <lifengqi@swartz.cn>
2018-09-12 19:45:32 -0700

    测试文件归类

--- AirTicket/items.py
+++ AirTicket/items.py
@@ -14,7 +14,7 @@ class AirticketItem(scrapy.Item):
     source = scrapy.Field()  # 来源（１去哪，２携程，３飞猪......）
     date = scrapy.Field()  # 日期
     starting_station = scrapy.Field()  # 始发站
-    starting_terminal = scrapy.Field()  #　始发航站楼
+    starting_terminal = scrapy.Field()  # 始发航站楼
     starting_time = scrapy.Field()  # 当地出发时间
     destination_station = scrapy.Field()  # 终点站
     destination_terminal = scrapy.Field()  # 终点航站楼
@@ -22,8 +22,8 @@ class AirticketItem(scrapy.Item):
     transfer = scrapy.Field()  # 中转站：[{'中转地':['到达时间']}]???
     price = scrapy.Field()  # 票价
     airline = scrapy.Field()  # 航空公司
-    is_non_stop = scrapy.Field()  #　是否直飞，１是，０否
-    total_time = scrapy.Field()  #　总耗时
+    is_non_stop = scrapy.Field()  # 是否直飞，１是，０否
+    total_time = scrapy.Field()  # 总耗时
     discount_rate = scrapy.Field()  # 折扣率
 
 
